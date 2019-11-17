ec385d47723e716caa4eb6420e6cc966c367617f
Amanda Rolon <abrolon87@gmail.com>
2019-11-17 04:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,16 @@ def roll_call_dwarves(dwarves)
 end
 
 roll_call_dwarves(dwarves)
+#passed
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+planeteer_calls = %w[earth wind fire water heart]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
+
+summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls# code an argument here
   # Your code here
