243cd783aea690e5ed1c2aaf9b386576d92d84e7
Amanda Rolon <abrolon87@gmail.com>
2019-11-17 04:30:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  hash = Hash.new
+  %w(array).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}# code an argument here
   # Your code here
 end
 
