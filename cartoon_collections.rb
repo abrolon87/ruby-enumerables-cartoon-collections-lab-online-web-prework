8a3b8d89136e176678e9a39c24d04e4295987056
Amanda Rolon <abrolon87@gmail.com>
2019-11-17 04:48:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,7 +39,7 @@ long_planeteer_calls(planeteer_calls)
 
 # the array below is here to help
 #cheese_types = ["cheddar", "gouda", "camembert"]
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+potentially_cheesy_items = %w[yogurt spinach cheddar coffee]
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
